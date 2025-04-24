ba03d5f2e7733ef65fed01afd63e4758a939625f
Breno Santiago De Sousa <dev.santiago@proton.me>
2025-04-23 23:46:53 -0300

    init

---
+++ app/src/main/java/com/example/pomoapp/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.pomoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import androidx.compose.ui.tooling.preview.Preview
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+                PomoApp()
+        }
+    }
+}
+
+@Composable
+fun PomoApp() {
+    var timeLeft by remember { mutableStateOf(25 * 60) }
+    var isRunning by remember { mutableStateOf(false) }
+
+    LaunchedEffect(isRunning) {
+        while (isRunning && timeLeft > 0) {
+            delay(1000)
+            timeLeft--
+        }
+
+    }
+
+    val minutes = timeLeft / 60
+    val seconds = timeLeft % 60
+    
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = String.format("%02d:%02d", minutes, seconds),
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(onClick = { isRunning = !isRunning }) {
+            Text(if (isRunning) "Pause" else "Start")
+        }
+    }
+
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+        PomoApp()
+}
