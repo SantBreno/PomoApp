ad3931a370b80716277ab3606f55a9aea3f03bff
Breno Santiago De Sousa <dev.santiago@proton.me>
2025-04-24 23:35:59 -0300

    feat: Add timer controls and session tracking
    - Added `StartPauseButton` and `ResetButton` with enhanced styling.
    - Implemented `sessionsPassed` state to track completed sessions.
    - Displayed the number of completed sessions.
    - Set the button colors and borders.

--- app/src/main/java/com/example/pomoapp/MainActivity.kt
+++ app/src/main/java/com/example/pomoapp/MainActivity.kt
@@ -3,20 +3,39 @@ package com.example.pomoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
-import androidx.compose.ui.tooling.preview.Preview
 import java.util.Locale
 
 
@@ -38,6 +57,8 @@ enum class TimerMode {
 fun PomoApp() {
     var focusInput by remember { mutableStateOf("25") }
     var breakInput by remember { mutableStateOf("5") }
+    var sessionsPassed by remember { mutableIntStateOf(0) }
+
 
     val focusMinutes = focusInput.toIntOrNull()?.coerceIn(1, 90) ?: 25
     val maxBreakMinutes = (focusMinutes - 1).coerceAtLeast(1)
@@ -52,23 +73,26 @@ fun PomoApp() {
     val cardColor = if (mode == TimerMode.WORK) Color(0xFFE83944) else Color(0xFF81D4FA)
 
     // When mode changes, reset the timer based on the mode
-    LaunchedEffect(isRunning) {
-        timeLeft = if (mode == TimerMode.WORK) focusMinutes * 60 else breakMinutes * 60
-    }
-
     // Timer CountDown Logic
     LaunchedEffect(isRunning) {
         while (isRunning && timeLeft > 0) {
             delay(1000)
             timeLeft--
         }
-        if (timeLeft == 0) {
+        if (timeLeft == 0 && isRunning) {
             isRunning = false
+
+            // Switch mode
             mode = if (mode == TimerMode.WORK) TimerMode.BREAK else TimerMode.WORK
 
+            // Increment sessions passed
+            if (mode == TimerMode.WORK) {
+                sessionsPassed++
+            }
+            // When mode changes, reset the timer based on the mode
+            timeLeft = if (mode == TimerMode.WORK) focusMinutes * 60 else breakMinutes * 60
         }
     }
-
 
     Column(
         modifier = Modifier
@@ -96,13 +120,24 @@ fun PomoApp() {
                 .padding(16.dp)
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(10.dp))
 
         // Timer Display
         TimerCard(minutes, seconds,cardColor)
 
+        Spacer(modifier = Modifier.height(10.dp))
 
-        Spacer(modifier = Modifier.height(24.dp))
+        Row(modifier = Modifier
+        ) {
+            Text(
+                text = "Sessions Completed: $sessionsPassed",
+                fontSize = 16.sp,
+                modifier = Modifier.padding(top = 8.dp),
+                color = Color.Gray
+            )
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
 
         // CONTROLS
 
@@ -166,15 +201,35 @@ fun TimerControls(
 
 @Composable
 fun StartPauseButton(isRunning: Boolean, onClick: () -> Unit) {
-    Button(onClick = onClick) {
-        Text(if (isRunning) "Pause" else "Start")
+    Button(
+        onClick = ( onClick ),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFFEE7879),
+            contentColor = Color.White
+        ),
+        border = BorderStroke(2.dp, Color(0xFFE83944))
+    ) {
+        Text(
+            if (isRunning) "Pause" else "Start",
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
 @Composable
 fun ResetButton(onClick: () -> Unit) {
-    Button(onClick = onClick) {
-        Text("Reset")
+    Button(
+        onClick = ( onClick ),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFFEE7879),
+            contentColor = Color.White
+        ),
+        border = BorderStroke(2.dp, Color(0xFFE83944))
+    ) {
+        Text(
+            "Reset",
+            fontWeight = FontWeight.Bold
+        )
     }
 
 }
